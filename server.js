4b2baf80cd060b3db66828d456136144159827c3
c1337 <cyril199897@gmail.com>
2022-05-05 14:56:17 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const userRouter = require("./routes/user-api");
+const requestRouter = require("./routes/request-api");
+const app = express();
+require('dotenv').config()
+const url  = "mongodb://root:example@127.0.0.1:27017/test?authSource=admin"
+
+// Connection to mongodb
+console.log("url is",url);
+mongoose.connect(url, {useNewUrlParser : true});
+
+// Middleware to handle body parser
+app.use(bodyParser.json());
+
+
+
+// Middleware to handle User request
+app.use('/api/user', userRouter);
+
+// Middleware to handle driver request
+app.use('/api/request', requestRouter);
+
+
+// port
+app.listen(8000,() => { 
+    console.log("Server running in port 8000");
+}); 
